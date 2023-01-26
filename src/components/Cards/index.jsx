30f069bdbdfc158f4ac879e77ff0c9ded8957ca0
Chiara Cirrito <chiara.cirrito1602@gmail.com>
2023-01-26 18:39:58 +0100

    update style-26/01

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,13 +1,12 @@
 import styles from "./styles.module.scss";
 import { useState } from "react";
 import { PUT } from "../../libs/HTTP";
+import { TbEdit, TbTrash } from "react-icons/tb";
 
 const Cards = ({ item, deleteElement, reloadItems }) => {
   const [formData, setFormData] = useState({
     title: item.title,
     price: item.price,
-    description: item.description,
-    categoryId: item.category.id,
     images: [item.images[0]],
   });
   const [showForm, setShowForm] = useState(false);
@@ -42,38 +41,32 @@ const Cards = ({ item, deleteElement, reloadItems }) => {
   return (
     <div className={styles.main}>
       <img src={item.images[0]} alt={item.title} />
-      <li onClick={() => deleteElement(item.id)}>{item.title}</li>
-      <button onClick={() => setShowForm(!showForm)}>Edit</button>
-      {showForm && (
-        <form onSubmit={(e) => submitForm(e)}>
-          <input
-            value={formData.title}
-            onChange={(e) => handleForm("title", e)}
-            type="text"
-          />
-          <input
-            value={formData.price}
-            onChange={(e) => handleForm("price", e)}
-            type="text"
-          />
-          <input
-            value={formData.description}
-            onChange={(e) => handleForm("description", e)}
-            type="text"
-          />
-          <input
-            value={formData.categoryId}
-            onChange={(e) => handleForm("categoryId", e)}
-            type="text"
-          />
-          <input
-            value={formData.images[0]}
-            onChange={(e) => handleForm("images", e)}
-            type="text"
-          />
-          <input type="submit" value="invia" />
-        </form>
-      )}
+      <h5>{item.title}</h5>
+      <h5>{item.price}$</h5>
+      <div className={styles.btn}>
+        <TbTrash onClick={() => deleteElement(item.id)}>{item.title}</TbTrash>{" "}
+        <TbEdit onClick={() => setShowForm(!showForm)}></TbEdit>
+        {showForm && (
+          <form onSubmit={(e) => submitForm(e)}>
+            <input
+              value={formData.title}
+              onChange={(e) => handleForm("title", e)}
+              type="text"
+            />
+            <input
+              value={formData.price}
+              onChange={(e) => handleForm("price", e)}
+              type="text"
+            />
+            <input
+              value={formData.images[0]}
+              onChange={(e) => handleForm("images", e)}
+              type="text"
+            />
+            <input type="submit" value="invia" />
+          </form>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,22 +3,34 @@ import styles from "./styles.module.scss";
 const Home = () => {
   return (
     <div className={styles.main}>
-      <p>
-        Hello! Ut autem eius aut sint quod et aliquid inventore ut quia eligendi
-        sit atque impedit et mollitia tempora sed iste corporis. Est dolorem
-        placeat et dolor quidem hic quas officiis nam mollitia quae et libero
-        esse non odit fuga sit error doloremque. Et dolores deleniti sit quaerat
-        veritatis ad temporibus ipsa aut voluptas eius rem officiis doloremque
-        eos iste similique. Et quibusdam pariatur aut minus dolore non ratione
-        quos non quibusdam consequatur et cupiditate natus!
-      </p>
-      <img src="https://picsum.photos/400/200" alt="img" />
-      <p>
-        Eum blanditiis quod 33 facilis quod et inventore veritatis ut mollitia
-        recusandae et aspernatur cumque. Sed dolor quae sit minus rerum aut
-        dolorum numquam. Et veniam natus id voluptatem quis aut nobis officia
-        eos itaque autem.
-      </p>
+      <div className={styles.text}>
+        <p>
+          Hello! Ut autem eius aut sint quod et aliquid inventore ut quia
+          eligendi sit atque impedit et mollitia tempora sed iste corporis. Est
+          dolorem placeat et dolor quidem hic quas officiis nam mollitia quae et
+          libero esse non odit fuga sit error doloremque. Et dolores deleniti
+          sit quaerat veritatis ad temporibus ipsa aut voluptas eius rem
+          officiis doloremque eos iste similique. Et quibusdam pariatur aut
+          minus dolore non ratione quos non quibusdam consequatur et cupiditate
+          natus!
+        </p>
+      </div>
+      <div className={styles.img}>
+        <img src="https://picsum.photos/200/200" alt="img" />
+        <img src="https://picsum.photos/200/200" alt="img" />
+        <img src="https://picsum.photos/200/200" alt="img" />
+      </div>
+      <div className={styles.description}>
+        <p>
+          Eum blanditiis quod 33 facilis quod et inventore veritatis ut mollitia
+          recusandae et aspernatur cumque. Sed dolor quae sit minus rerum aut
+          dolorum numquam. Et veniam natus id voluptatem quis aut nobis officia
+          eos itaque autem. Et dolores deleniti sit quaerat veritatis ad
+          temporibus ipsa aut voluptas eius rem officiis doloremque eos iste
+          similique. Et quibusdam pariatur aut minus dolore non ratione quos non
+          quibusdam consequatur et cupiditate natus!
+        </p>
+      </div>
     </div>
   );
 };
