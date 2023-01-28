733ab04927e4eaf2794b8a60876154fb43913637
Chiara Cirrito <chiara.cirrito1602@gmail.com>
2023-01-28 14:00:49 +0100

    Home- 27/01/23

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,35 +1,61 @@
 import styles from "./styles.module.scss";
+import styled from "styled-components";
 
 const Home = () => {
+  const Container = styled.div`
+    display: flex;
+    justify-conter: center;
+    align-items: center;
+    gap: 5px;
+    margin: 100px;
+    flex-wrap: wrap;
+  `;
+
+  const Popular = styled.div`
+    display: flex;
+    justify-conter: center;
+    align-items: center;
+    flex-wrap: wrap;
+    margin-top: 40px;
+    gap: 30px;
+  `;
+
   return (
     <div className={styles.main}>
-      <div className={styles.text}>
-        <p>
-          Hello! Ut autem eius aut sint quod et aliquid inventore ut quia
-          eligendi sit atque impedit et mollitia tempora sed iste corporis. Est
-          dolorem placeat et dolor quidem hic quas officiis nam mollitia quae et
-          libero esse non odit fuga sit error doloremque. Et dolores deleniti
-          sit quaerat veritatis ad temporibus ipsa aut voluptas eius rem
-          officiis doloremque eos iste similique. Et quibusdam pariatur aut
-          minus dolore non ratione quos non quibusdam consequatur et cupiditate
-          natus!
-        </p>
-      </div>
-      <div className={styles.img}>
-        <img src="https://picsum.photos/300/300" alt="img" />
-        <img src="https://picsum.photos/200/200" alt="img" />
-        <img src="https://picsum.photos/200/200" alt="img" />
-      </div>
-      <div className={styles.description}>
-        <p>
-          Eum blanditiis quod 33 facilis quod et inventore veritatis ut mollitia
-          recusandae et aspernatur cumque. Sed dolor quae sit minus rerum aut
-          dolorum numquam. Et veniam natus id voluptatem quis aut nobis officia
-          eos itaque autem. Et dolores deleniti sit quaerat veritatis ad
-          temporibus ipsa aut voluptas eius rem officiis doloremque eos iste
-          similique. Et quibusdam pariatur aut minus dolore non ratione quos non
-          quibusdam consequatur et cupiditate natus!
-        </p>
+      <Container>
+        <h1>Your Favorite Shop!</h1>
+        <img
+          src="https://www.elimeli.it/wp-content/uploads/2021/01/Come-piegare-i-vestiti-in-valigia-per-risparmiare-spazio-1160x870.jpg"
+          alt="img"
+        />
+      </Container>
+      <h5>Popular products</h5>
+      <Popular>
+        <h6>#Shoes</h6>
+        <h6>#Nike</h6>
+        <h6>#Sport</h6>
+      </Popular>
+      <div className={styles.images}>
+        <img
+          src="https://m.media-amazon.com/images/I/51L8QJ2z1SL._AC_SY625._SX._UX._SY._UY_.jpg"
+          alt="img"
+        />
+        <img
+          src="https://m.media-amazon.com/images/I/51+B0XrcJDL._AC_UY625_.jpg"
+          alt="img"
+        />
+        <img
+          src="https://m.media-amazon.com/images/I/51anuZXrunL._AC_SY625._SX._UX._SY._UY_.jpg"
+          alt="img"
+        />
+        <img
+          src="https://m.media-amazon.com/images/I/41KbvAae7DL._AC_UY625_.jpg"
+          alt="img"
+        />
+        <img
+          src="https://m.media-amazon.com/images/I/71fpMl1DmbS._AC_SX625._SX._UX._SY._UY_.jpg"
+          alt="img"
+        />
       </div>
     </div>
   );
